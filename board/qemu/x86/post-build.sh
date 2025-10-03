67bd2becc50ba81e31b6932cbe664c6eed30543e
Fernando Neto <136604948+netozgn@users.noreply.github.com>
2025-10-03 21:50:51 +0000

    Short env fixes

--- board/qemu/x86/post-build.sh
+++ board/qemu/x86/post-build.sh
@@ -9,10 +9,3 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
     sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # QEMU graphical window' ${TARGET_DIR}/etc/inittab
 fi
-
-BUILDROOT_DIR=$BASE_DIR/..
-COMPILER=$BUILDROOT_DIR/output/host/bin/i686-buildroot-linux-gnu-gcc
-$COMPILER -o $BUILDROOT_DIR/output/target/bin/syscall_test $BUILDROOT_DIR/custom-scripts/syscall_test.c
-
-$COMPILER -o $BUILDROOT_DIR/output/target/bin/syscall_desafio1_processSleep $BUILDROOT_DIR/custom-scripts/syscall_desafio1_processSleep.c
-$COMPILER -o $BUILDROOT_DIR/output/target/bin/syscall_processListenMessage $BUILDROOT_DIR/custom-scripts/syscall_processListenMessage.c
--- start_qemu.sh
+++ start_qemu.sh
@@ -1,1 +1,1 @@
-sudo qemu-system-i386 --kernel output/images/bzImage --hda output/images/rootfs.ext2 --hdb sdb.bin --nographic --append "console=ttyS0 root=/dev/sda"
+sudo qemu-system-i386 --kernel output/images/bzImage --hda output/images/rootfs.ext2 --nographic --append "console=ttyS0 root=/dev/sda"
